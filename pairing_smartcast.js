52b87d446a308ce03d2182a69717345f861874bc
Randy Ramsaywack <randyramsaywack@me.com>
2020-01-23 13:07:20 -0500

    Script for pairing TV

---
+++ pairing_smartcast.js
@@ -0,0 +1,23 @@
+let smartcast = require('vizio-smart-cast');
+let readline = require('readline'); // user input via cmd line
+let tv = new smartcast('PUT TV IP HERE');
+
+// configure cmd line input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Initiate a pairing request with a smartcast device
+tv.pairing.initiate().then(response => {
+  // prompt the user for the pin that is displayed on the smartcast device
+  rl.question('Enter PIN:', answer => {
+    // send the pin to the smartcast device to complete the pairing process
+    tv.pairing.pair(answer).then(response => {
+      // log the token to be used for future, authenticated requests
+      console.log(response.ITEM.AUTH_TOKEN);
+    });
+
+    rl.close();
+  });
+});
